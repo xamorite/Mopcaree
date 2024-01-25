da8e0ce837262077f71f47a574dd14f6aafa9bd6
Xamorite <eogunneye@gmail.com>
2024-01-25 23:36:26 +0100

    started services

--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -1,10 +1,18 @@
 export default function Service() {
   return (
-    <div className="mx-auto px-6 text-center">
+    <div className="mx-auto w-[80vw] mt-[10vh] text-center">
       <div>
         <header className="text-[#3379C1] font-bold text-[4vw] md:text-[1.5vw]">Services</header><br />
-        <h1 className="  font-bold md:text-[2vw] text-[5vw]">MOPCARE DIRECT INTERVENTIONS</h1>
+        <h3 className="  font-bold md:text-[4vw] text-[6vw]">MOPCARE DIRECT INTERVENTIONS</h3>
       </div>
+
+
+      <div className="mt-[10vh]">
+        <h1 className="font-semibold md:text-[3vw] mb-[5vh]  text-[6vw]">MOPCARE SENIORS CONNECT</h1>
+        <p className="md:text-[1.5vw] md:text-center text-[#667085] text-start">A monthly gathering of seniors where they receive free basic medical checks, nutritional and psychological counselling, pharmaceutical care, food supplements, antioxidants and commodities of daily living etc</p>
+      </div>
+
+
     </div>
   )
 }
